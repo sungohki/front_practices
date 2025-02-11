08aec7cadcabe778a8b171f8219c1766782c6422
sungohki <sokim1201@naver.com>
2025-02-11 14:57:15 +0900

    feat: Add category pages for each

--- mobile_design_practice/customKeyboard/scripts/categoryEvent.js
+++ mobile_design_practice/customKeyboard/scripts/categoryEvent.js
@@ -4,21 +4,29 @@ const switchBtn = document.querySelector('#footer .category:nth-child(2)');
 const housingBtn = document.querySelector('#footer .category:nth-child(3)');
 const estimateBtn = document.querySelector('#footer .category:nth-child(4)');
 const profileBtn = document.querySelector('#footer .category:nth-child(5)');
+const host = window.location.host;
 
 // Events
+homeBtn.addEventListener('click', function () {
+  if (!checkCurrentPage(this)) window.open('../index.html', '_self');
+});
+switchBtn.addEventListener('click', function () {
+  if (!checkCurrentPage(this)) window.open('../pages/switchList.html', '_self');
+});
+housingBtn.addEventListener('click', function () {
+  if (!checkCurrentPage(this))
+    window.open('../pages/housingList.html', '_self');
+});
+estimateBtn.addEventListener('click', function () {
+  if (!checkCurrentPage(this))
+    window.open('../pages/estimatePage.html', '_self');
+});
+profileBtn.addEventListener('click', function () {
+  if (!checkCurrentPage(this))
+    window.open('../pages/profilePage.html', '_self');
+});
 
-homeBtn.addEventListener('click', () => {
-  window.open('../index.html', '_self');
-});
-switchBtn.addEventListener('click', () => {
-  window.open('../pages/switchList.html', '_self');
-});
-housingBtn.addEventListener('click', () => {
-  window.open('../pages/housingList.html', '_self');
-});
-estimateBtn.addEventListener('click', () => {
-  window.open('../pages/estimatePage.html', '_self');
-});
-profileBtn.addEventListener('click', () => {
-  window.open('../pages/profilePage.html', '_self');
-});
+// Functions
+function checkCurrentPage(element) {
+  return element.classList.contains('on');
+}
