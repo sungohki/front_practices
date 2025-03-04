5a309a526b0fac162ff7102ffbc8ac1e72f36881
sungohki <sokim1201@naver.com>
2025-03-04 17:36:26 +0900

    feat: Add input required mark

--- src/cardEvents.js
+++ src/cardEvents.js
@@ -9,7 +9,8 @@ window.addEventListener('scroll', () => {
   if (aboutSection.getBoundingClientRect().y < 400) {
     aboutCard.classList.add('on');
   }
-  if (featureSection.getBoundingClientRect().y < 600) {
+  console.log(featureSection.getBoundingClientRect().y);
+  if (featureSection.getBoundingClientRect().y < 800) {
     featureCards.forEach((item, index) => {
       item.style.animation = `.4s swing .${index}s forwards, .4s appear .${index}s forwards`;
     });
