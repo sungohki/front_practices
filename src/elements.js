03fcb623e614759740fd5a91c0b3673832be92f1
sungohki <sokim1201@naver.com>
2025-03-08 16:57:26 +0900

    feat: Refactoring features box

--- src/elements.js
+++ src/elements.js
@@ -1,22 +1,24 @@
 // Section Elements
-const headerTab = document.querySelector('#header');
-const bannerSection = document.querySelector('#banner');
-const mainSection = document.querySelector('#main');
-const aboutBox = mainSection.querySelector('.aboutBox');
-const featuresBox = mainSection.querySelector('.featuresBox');
+const headerTab = document.querySelector("#header");
+const bannerSection = document.querySelector("#banner");
+const mainSection = document.querySelector("#main");
+const aboutBox = mainSection.querySelector(".about_box");
+const featuresBox = mainSection.querySelector(".features_box");
 
 const bannerTypingSpace = bannerSection.querySelector(
-  '.banner_greeting_card > p:nth-child(2) > span'
+  ".banner_greeting_card > p:nth-child(2) > span",
 );
-const bannerSwipingButton = bannerSection.querySelector('.banner_swipe_button');
+const bannerSwipingButton = bannerSection.querySelector(".banner_swipe_button");
 
-const aboutCard = aboutBox.querySelector('.about_box_profile > .frame');
-const featureCards = featuresBox.querySelectorAll('.stack > .item');
+const aboutCard = aboutBox.querySelector(".about_box_profile > .frame");
+const featureCards = featuresBox.querySelectorAll(
+  ".features_box_list > .features_box_item",
+);
 
 const sendForm = document.querySelector(
-  '#contact > .card > .contact > .contact_form'
+  "#contact > .card > .contact > .contact_form",
 );
-const senderName = sendForm.querySelector('.sender_name');
-const senderEmail = sendForm.querySelector('.sender_email');
-const senderMessage = sendForm.querySelector('.sender_message');
-const sendBtn = sendForm.querySelector('.send_btn');
+const senderName = sendForm.querySelector(".sender_name");
+const senderEmail = sendForm.querySelector(".sender_email");
+const senderMessage = sendForm.querySelector(".sender_message");
+const sendBtn = sendForm.querySelector(".send_btn");
