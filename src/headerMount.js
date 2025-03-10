7da8d890bc09d956b4d5e41cfdbe749f65bd7b0c
sungohki <sokim1201@naver.com>
2025-03-10 12:25:19 +0900

    chore: Clean code

--- src/headerMount.js
+++ src/headerMount.js
@@ -1,7 +1,9 @@
 // Events
 window.addEventListener('scroll', () => {
   // console.log(headerTop.getBoundingClientRect());
-  if (mainSection.getBoundingClientRect().y - 40 <= 0)
+  if (mainSection.getBoundingClientRect().y - 40 <= 0) {
     headerTab.classList.add('unmount');
-  else headerTab.classList.remove('unmount');
+  } else {
+    headerTab.classList.remove('unmount');
+  }
 });
