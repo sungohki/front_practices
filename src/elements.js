4ee88f98102b34bed3cbab3c758d4d6bbd749ad7
sungohki <sokim1201@naver.com>
2025-03-10 11:05:56 +0900

    fix : Fix send form alert function

--- src/elements.js
+++ src/elements.js
@@ -1,24 +1,23 @@
 // Section Elements
-const headerTab = document.querySelector("#header");
-const bannerSection = document.querySelector("#banner");
-const mainSection = document.querySelector("#main");
-const aboutBox = mainSection.querySelector(".about_box");
-const featuresBox = mainSection.querySelector(".features_box");
+const headerTab = document.querySelector('#header');
+const bannerSection = document.querySelector('#banner');
+const mainSection = document.querySelector('#main');
+const aboutBox = mainSection.querySelector('.about_box');
+const featuresBox = mainSection.querySelector('.features_box');
+const contactBox = mainSection.querySelector('.contact_box');
 
 const bannerTypingSpace = bannerSection.querySelector(
-  ".banner_greeting_card > p:nth-child(2) > span",
+  '.banner_greeting_card > p:nth-child(2) > span'
 );
-const bannerSwipingButton = bannerSection.querySelector(".banner_swipe_button");
+const bannerSwipingButton = bannerSection.querySelector('.banner_swipe_button');
 
-const aboutCard = aboutBox.querySelector(".about_box_profile > .frame");
+const aboutCard = aboutBox.querySelector('.about_box_profile > .frame');
 const featureCards = featuresBox.querySelectorAll(
-  ".features_box_list > .features_box_item",
+  '.features_box_list > .features_box_item'
 );
 
-const sendForm = document.querySelector(
-  "#contact > .card > .contact > .contact_form",
-);
-const senderName = sendForm.querySelector(".sender_name");
-const senderEmail = sendForm.querySelector(".sender_email");
-const senderMessage = sendForm.querySelector(".sender_message");
-const sendBtn = sendForm.querySelector(".send_btn");
+const sendForm = contact.querySelector('.contact_form');
+const senderName = sendForm.querySelector('.sender_name');
+const senderEmail = sendForm.querySelector('.sender_email');
+const senderMessage = sendForm.querySelector('.sender_message');
+const sendBtn = sendForm.querySelector('.send_btn');
